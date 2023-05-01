95f8c4d90eff21068bf4e64ab3777f79bcc6c71f
subhamwork99 <subhamwork.ojha@gmail.com>
2023-05-01 15:04:53 +0530

    mongodb data display

--- web/frontend/pages/products.jsx
+++ web/frontend/pages/products.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useAppQuery } from "../hooks";
+import { useEffect, useState } from "react"
 
 function products() {
-    const {data} = useAppQuery({url: "/api/products"});
-    console.log("data",data);
+    // const {data} = useAppQuery({url: "/api/products"});
+    // console.log("data",data);
+    useEffect(()=>{
+        getResult()
+      })
+
+    const getResult = async () => {
+        let showResult = await fetch('http://localhost:8081/api/products')
+        showResult = await showResult.json()
+        console.log("showResult",showResult);
+    }
+
 
   return (
     <div>products</div>
